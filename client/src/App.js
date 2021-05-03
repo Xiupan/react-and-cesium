3ebb9f4b8ed6c49bf835d16e030302f9d04d3e71
Alan Hong <xiupan@gmail.com>
2021-05-03 13:15:06 -0500

    feat: demo with multiple dummy satellites and their animated orbits

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { Viewer, CzmlDataSource } from 'resium';
 import czml from 'czml-writer';
+import _ from 'lodash';
 
 const App = () => {
-  const orbit = new czml.orbit.fromParams({
-    apogee: 426.9, // km
-    perigee: 416.2, // km
-    inclination: 51.65, // deg
-    rightAscension: 304.1, // deg
-    argumentOfPeriapsis: 117.8, // deg
-  });
-  const output = orbit.czml();
-  console.log(output);
+  let satelliteData = [];
+  // add 5 dummy satellites
+  for (let index = 0; index < 5; index++) {
+    const newSatellite = {
+      apogee: _.random(400.0, 500.0), // km
+      perigee: _.random(400.0, 500.0), // km
+      inclination: _.random(0.0, 90.0), // deg
+      rightAscension: _.random(0.0, 360.0), // deg
+      argumentOfPeriapsis: _.random(0.0, 360.0), // deg
+    };
+    const newOrbit = new czml.orbit.fromParams(newSatellite);
+    const newCzml = newOrbit.czml();
+
+    const formattedName = `Satellite-${index + 1}`;
+
+    if (index === 0) {
+      newCzml[1].id = formattedName;
+      newCzml[1].label.text = formattedName;
+      satelliteData = [...satelliteData, ...newCzml];
+    }
+    newCzml[1].id = formattedName;
+    newCzml[1].label.text = formattedName;
+    satelliteData = [...satelliteData, newCzml[1]];
+  }
 
   return (
     <Viewer shouldAnimate full>
-      <CzmlDataSource data={output} />
+      <CzmlDataSource data={satelliteData} />
     </Viewer>
   );
 };
